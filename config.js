35eb39af6d2f3ba6c8b0fc8a2f00056c048cb31d
Ubuntu <ubuntu@ip-172-31-15-18.us-west-1.compute.internal>
2016-10-20 09:22:01 +0000

    V5.6 -Removed all uncessary files

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    debug: false
-  , coinbase: {
-        key: '4VirhF0Y0XIt3pDb'
-      , rate: 5000 // retry interval, in milliseconds.  Please be nice.
-      , logfile: 'trader.log'
-  }
-}
